f5fe79ee5abd57181ddc0b3c9a85d312c27a9398
oyeong011 <oyeong011@gmail.com>
2025-08-11 15:50:42 +0900

    Initial Commit

---
+++ YearProgressTests/YearProgressTests.swift
@@ -0,0 +1,16 @@
+//
+//  YearProgressTests.swift
+//  YearProgressTests
+//
+//  Created by 권오영 on 8/11/25.
+//
+
+import Testing
+
+struct YearProgressTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
